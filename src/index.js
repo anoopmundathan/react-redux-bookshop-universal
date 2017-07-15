291aab3bc92ccca4aa281eacf8b8e3931a156129
anoopmundathan <anoop.mundathan@gmail.com>
2017-07-15 22:05:28 +0100

    Setup route

--- src/index.js
+++ src/index.js
@@ -1,25 +1,20 @@
-
 // REACT
 import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
 
 // REACT ROUTER
-import {
-	BrowserRouter as Router,
-	Route,
-	Link
-} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import {applyMiddleware, createStore} from 'redux';
-import { createLogger } from 'redux-logger';
+import {createLogger} from 'redux-logger';
 import thunk from 'redux-thunk';
 
 import reducers from './reducers';
 import {postBooks, deleteBooks, updateBooks} from './actions/bookActions';
 import {addToCart} from './actions/cartActions';
 
-import Menu from './components/Menu';
-import Footer from './components/Footer';
+import Menu from './components/menu';
+import Footer from './components/footer';
 import BookList from './components/pages/bookList';
 
 // Create middleware
@@ -28,11 +23,18 @@ const middleware = applyMiddleware(thunk,createLogger());
 // step1 : create store
 const store = createStore(reducers, middleware);
 
-render(
+const Routes = (
 	<Provider store={store}>
-		<div>
-			<Menu /> 
-			<Footer />
-		</div>
-	</Provider>, document.getElementById('root')
+		<Router>
+			<div>
+				<Route exact path="/" component={Menu} />
+				<Route exact path="/" component={BookList} />
+				<Route exact path="/" component={Footer} />
+			</div>
+		</Router>
+	</Provider>
+);
+
+render(
+	Routes, document.getElementById('root')
 );
