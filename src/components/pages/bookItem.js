257ae5081e1c40f93b55b3dd6e5bdd06e5a2f61e
anoopmundathan <anoop.mundathan@gmail.com>
2017-07-23 07:38:44 +0100

    Add long description

--- src/components/pages/bookItem.js
+++ src/components/pages/bookItem.js
@@ -9,6 +9,17 @@ import {postBooks} from '../../actions/bookActions';
 
 class BookItem extends Component {
 	
+	constructor() {
+		super();
+		this.state = {
+			isClicked: false
+		};
+	}
+
+	onReadMore() {
+		this.setState({isClicked: true});
+	}
+
 	handleClick() {
 		const book = [...this.props.cart, {
 			id: this.props.id,
@@ -41,7 +52,21 @@ class BookItem extends Component {
 					</Col>
 					<Col xs={6} sm={8}>
 						<h6>{this.props.title}</h6>
-						<p>{this.props.description}</p>
+
+						<p>{(this.props.description.length > 50 &&
+							this.state.isClicked ===false)
+							? (this.props.description.substring(0,50))
+							: (this.props.description)}
+
+							<button className='link'
+							onClick={this.onReadMore.bind(this)}>
+								{(this.state.isClicked === false && 
+								this.props.description !== null &&
+								this.props.description.length > 50)
+								?('...readmore'):('')}
+							</button>
+						</p>
+
 						<h6>usd. {this.props.price}</h6>
 						<Button bsStyle='primary' 
 							onClick={this.handleClick.bind(this)}>Buy Now</Button>
