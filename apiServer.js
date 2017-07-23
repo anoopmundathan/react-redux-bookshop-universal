35c18d31a4430bc16f4f9cfe5b5053083db5041c
anoopmundathan <anoop.mundathan@gmail.com>
2017-07-23 13:59:50 +0100

    Use environment variable

--- apiServer.js
+++ apiServer.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 
 // API
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/bookshop');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/bookshop');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, '#MongoDB - connection error: '));
 
